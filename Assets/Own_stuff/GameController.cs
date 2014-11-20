43c50b85a27ca62b002a9890395874787049fa34
Niklas Barning <niklas.barning@hfk-bremen.de>
2014-11-20 14:02:40 +0100

    New Room and Room Switching

---
+++ Assets/Own_stuff/GameController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Fire1")) {
+			loadLevel();
+		}
+	}
+
+	void loadLevel() {
+		if (Application.loadedLevel == 0) {
+			Application.LoadLevel ("Wald");
+			} else {
+			Application.LoadLevel ("Room");
+				}
+	}
+}
