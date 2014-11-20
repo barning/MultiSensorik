947221e60af717a9a89b84993b6043ab84d0b270
Niklas Barning <niklas.barning@hfk-bremen.de>
2014-11-20 17:42:03 +0100

    New Audio and Level Switching

--- Assets/Own_stuff/GameController.cs
+++ Assets/Own_stuff/GameController.cs
@@ -3,12 +3,23 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
+	public AudioClip Welcome;
+	public AudioClip Explain;
+	public AudioClip Ready;
+	public AudioClip OkGo;
+	public int voiceCounter = 0;
+
 	// Use this for initialization
 	void Start () {
 	}
 
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetButtonDown("Fire3")) {
+			theSpeaker(voiceCounter);
+			voiceCounter++;
+			print(voiceCounter);
+		}
 		if (Input.GetButton("Fire1")) {
 			loadLevel();
 		}
@@ -20,5 +31,21 @@ public class GameController : MonoBehaviour {
 			} else {
 			Application.LoadLevel ("Room");
 				}
+	}
+
+	void theSpeaker(int number) {
+
+		if (number == 0) {
+			audio.PlayOneShot(Welcome);
+		}
+		if (number == 1) {
+			audio.PlayOneShot(Explain);
+		}
+		if (number == 2) {
+			audio.PlayOneShot(Ready);
+		}
+		if (number == 3) {
+			audio.PlayOneShot(OkGo);
+		}
 	}
 }
