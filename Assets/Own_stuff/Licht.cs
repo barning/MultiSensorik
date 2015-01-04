ba982a108fa824bb5a75be4dc1f2b4870b859dd9
Niklas <moin@niklasbarning.de>
2015-01-04 19:46:20 +0100

    Skybox and Light changes

--- Assets/Own_stuff/Licht.cs
+++ Assets/Own_stuff/Licht.cs
@@ -3,26 +3,43 @@ using System.Collections;
 
 public class Licht : MonoBehaviour {
 	public GameObject licht;
-	float blender = 0f;
-	//float timer = Time.realtimeSinceStartup;
+	public float smooth;
+	float lastTimer;
+	bool night = false;
+	float randLight = 6f;
+
 	// Use this for initialization
 	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown ("y")) {
-			licht.light.intensity = 0f;
+
+		if (Time.realtimeSinceStartup > 5) {
+			licht.light.intensity = Mathf.Lerp(licht.light.intensity,0f,smooth * Time.deltaTime);
 			RenderSettings.fogColor = Color.black;
+			blendSkybox (1);
 				}
-		if (Input.GetKeyDown ("j")) {
-			blendSkybox(blender);
-			blender+=0.1f;
+		if (Time.realtimeSinceStartup > 30) {
+			night=true;
 				}
+		if (night == true) {
+			if (Time.realtimeSinceStartup - lastTimer > randLight){
+				licht.light.intensity = 8f;
+				licht.light.color = Color.white;
+				lastTimer = Time.realtimeSinceStartup;
+				randLight = Random.Range(1f,8f);
+				}
+			else {
+				licht.light.intensity = 0f;
+			}
+		}
 	
 	}
 
-	void blendSkybox(float blending){
-		RenderSettings.skybox.SetFloat ("_Blend", blending);
-	}
+	void blendSkybox (float blend) {
+		float temp = blend;
+		RenderSettings.skybox.SetFloat ("_Blend", temp);
+		}
+
 }
