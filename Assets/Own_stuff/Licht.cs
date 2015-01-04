641d4b6b1d7ef5bcab984e1d4a7d0e21bced037d
Niklas <moin@niklasbarning.de>
2015-01-04 17:36:15 +0100

    Worked on light and rain

---
+++ Assets/Own_stuff/Licht.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Licht : MonoBehaviour {
+	public GameObject licht;
+	float blender = 0f;
+	//float timer = Time.realtimeSinceStartup;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("y")) {
+			licht.light.intensity = 0f;
+			RenderSettings.fogColor = Color.black;
+				}
+		if (Input.GetKeyDown ("j")) {
+			blendSkybox(blender);
+			blender+=0.1f;
+				}
+	
+	}
+
+	void blendSkybox(float blending){
+		RenderSettings.skybox.SetFloat ("_Blend", blending);
+	}
+}
---
+++ Assets/Own_stuff/Unwetter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unwetter : MonoBehaviour {
+	public GameObject Wind1;
+	public GameObject Wind2;
+	public GameObject Wind3;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			Wind1.SetActive(false);
+			Wind2.SetActive(false);
+			Wind3.SetActive(false);
+				}
+		if (Input.GetKeyDown ("h")) {
+			Wind1.SetActive(true);
+			Wind2.SetActive(true);
+			Wind3.SetActive(true);
+				}
+	}
+}
