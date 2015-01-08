ef260765be626225794ef6ec7ba872b1a27bdf58
Niklas <moin@niklasbarning.de>
2015-01-09 00:29:29 +0100

    better graphics

---
+++ Assets/Own_stuff/CloudMove.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudMove : MonoBehaviour {
+
+	public float cloudSpeed;
+
+	void Update() {
+		float step = cloudSpeed * Time.deltaTime;
+		transform.position += Vector3.back * step;
+	}
+}
