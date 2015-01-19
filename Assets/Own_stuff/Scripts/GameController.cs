6eff8c46ef5f99a6d66d9c118f6df7cd080d356c
Niklas <moin@niklasbarning.de>
2015-01-19 13:53:17 +0100

    First Level Ready

--- Assets/Own_stuff/Scripts/GameController.cs
+++ Assets/Own_stuff/Scripts/GameController.cs
@@ -2,10 +2,18 @@
 using System.Collections;
 
 public class GameController : MonoBehaviour {
-
+	public AudioClip[] speakers;
+	public GameObject theCanvas;
+	int voiceCounter = 0;
+	int levelCounter;
+	bool wasPlayed = false;
 	// Use this for initialization
+
 	void Start () {
+		levelCounter = Application.loadedLevel +1;
+		print("Level is "+levelCounter);
 		Screen.showCursor = false;
+		wasPlayed = false;
 	}
 
 	// Update is called once per frame
@@ -13,13 +21,56 @@ public class GameController : MonoBehaviour {
 		if (Input.GetButton("Fire1")) {
 			loadLevel();
 		}
+		print (Time.timeSinceLevelLoad);
+
+		///THIS IS FOR LEVEL 1
+		if (Application.loadedLevel == 0)
+		{
+			if (!theCanvas.animation.isPlaying && !wasPlayed && Time.timeSinceLevelLoad >= 8){
+				theSpeaker(0);
+				wasPlayed = true;
+			}
+		}
+		if (levelCounter == 1 && Time.timeSinceLevelLoad >= 30) {
+			loadLevel();
+				}
+		/////////
+		/// 
+		///THIS IS FOR LEVEL 2
+		if (Application.loadedLevel == 1)
+		{
+			if (!wasPlayed && Time.timeSinceLevelLoad >= 8 && Time.timeSinceLevelLoad <= 9){
+				theSpeaker(0);
+				wasPlayed = true;
+				print("Erster");
+			}
+			if (Time.timeSinceLevelLoad >= 20 && Time.timeSinceLevelLoad <= 22){
+				wasPlayed = false;
+				print("Mitte");
+			}
+			if (!wasPlayed && Time.timeSinceLevelLoad >= 100){
+				theSpeaker(1);
+				wasPlayed = true;
+				print("Zweiter");
+			}
+		}
+		/*if (levelCounter == 2 && Time.timeSinceLevelLoad >= 30) {
+			loadLevel();
+		}*/
+		/////////
+		
+		if (Input.GetButtonDown("Fire3")) {
+			theSpeaker(voiceCounter);
+			voiceCounter++;
+			print(voiceCounter);
+		}
+	}
+	
+	void theSpeaker(int number) {
+		audio.PlayOneShot(speakers[number]);
 	}
 
 	void loadLevel() {
-		//AudioSource[] sounds = FindObjectsOfType (typeof(AudioSource))as AudioSource[];
-		//foreach (AudioSource audio in sounds) {
-		//	audio.Stop();
-		//		} 
 		if (Application.loadedLevel != 2){
 			int nextlevel = Application.loadedLevel + 1;
 			FadeOutMusic();
